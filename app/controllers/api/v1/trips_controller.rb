1fc6c8849e24e32319f131a7e1390c2b75f632f5
scott <you@example.comitsmescottb123@gmail.com>
2018-03-30 08:50:51 -0400

    added dates to trips

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -28,6 +28,6 @@ class Api::V1::TripsController < ApplicationController
   private
 
   def trip_params
-    params.require(:trip).permit(:name, :city, :state, :country, :user_id)
+    params.require(:trip).permit(:name, :city, :state, :country, :user_id, :start_date, :end_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327202106) do
+ActiveRecord::Schema.define(version: 20180329200619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20180327202106) do
     t.string "city"
     t.string "state"
     t.string "country"
+    t.date "start_date"
+    t.date "end_date"
   end
 
   create_table "users", force: :cascade do |t|
